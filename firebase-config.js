7eeeef0ae2a68ac137663cad83feb18ac303f7e2
p1cklethenut <146802142+p1cklethenut@users.noreply.github.com>
2023-10-03 16:21:05 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAmgt0H2Xp1xGftTBAigwwiT_h12wEdF6E",
+    authDomain: "messaging-cf1fb.firebaseapp.com",
+    projectId: "messaging-cf1fb",
+    storageBucket: "messaging-cf1fb.appspot.com",
+    messagingSenderId: "552719252144",
+    appId: "1:552719252144:web:59fb764a2e13dde78f1975",
+    measurementId: "G-GR5L50XFX8"
+};
+
+firebase.initializeApp(firebaseConfig);
